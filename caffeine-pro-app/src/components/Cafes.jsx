1ae92fb1b174fd33844889b5fba751acb8c01daf
Gabriela Cancello <70165237+gabycancello@users.noreply.github.com>
2025-03-14 12:13:08 -0300

    Update Cafes.jsx

--- caffeine-pro-app/src/components/Cafes.jsx
+++ caffeine-pro-app/src/components/Cafes.jsx
@@ -1,49 +1,49 @@
 const cafes = {
     feliz: {
     nome: "Cinnamon Dolce Latte",
-    imagem: "./public/images/cafes/cinnamon-dolce-latte.png",
+    imagem: "/images/cafes/cinnamon-dolce-latte.png",
     descricao: "Doce, aconchegante e envolvente, perfeito para quem está de bom humor e quer algo reconfortante.",
     link: "https://www.starbucks.com/menu/product/414/hot"
     },
     tranquilo: {
       nome: "Lavender Oatmilk Latte",
-      imagem: "./public/images/cafes/Lavender-Oatmilk-Lattee.png",
+      imagem: "/images/cafes/Lavender-Oatmilk-Lattee.png",
       descricao: "Floral e suave, ideal para momentos de calma e relaxamento.",
       link: "https://www.starbucks.com/menu/product/212/hot"
     },
     curioso: {
       nome: "Pistachio Latte",
-      imagem: "./public/images/cafes/pistachio-latte.png",
+      imagem: "/images/cafes/pistachio-latte.png",
       descricao: "Um sabor perfeito para quem gosta de experimentar novidades",
       link: "https://www.starbucks.com/menu/product/2123404/hot"
       },
     cansado: {
       nome: "Espresso",
-      imagem: "./public/images/cafes/double-espresso.png",
+      imagem: "/images/cafes/double-espresso.png",
       descricao: "Forte e intenso, perfeito para despertar.",
       link: "https://www.starbucks.com/menu/product/410/hot"
     },
     focado: {
       nome: "Flat White",
-      imagem: "./public/images/cafes/flat-white.png",
+      imagem: "/images/cafes/flat-white.png",
       descricao: "Cremosidade na medida certa para manter o foco.",
       link: "https://www.starbucks.com/menu/product/565/hot"
     },
     nervoso: {
       nome: "Cold Brew Nitro",
-      imagem: "./public/images/cafes/cold-brew-nitro.png",
+      imagem: "/images/cafes/cold-brew-nitro.png",
       descricao: "Suave, gelado e perfeito para relaxar.",
       link: "https://www.starbucks.com/menu/product/2121912/iced"
     },
     romantico: {
       nome: "White Chocolate Mocha",
-      imagem: "./public/images/cafes/white-chocolate-mocha.png",
+      imagem: "/images/cafes/white-chocolate-mocha.png",
       descricao: "Doce, envolvente e simplesmente irresistível",
       link: "https://www.starbucks.com/menu/product/2122724/iced"
     },
     geek: {
       nome: "Strawberry Crème Frappuccino",
-      imagem: "./public/images/cafes/strawberry-frapp.png",
+      imagem: "/images/cafes/strawberry-frapp.png",
       descricao: "Essa bebida é como um easter egg secreto no menu",
       link: "https://www.starbucks.com/menu/product/2122724/iced"
     }
